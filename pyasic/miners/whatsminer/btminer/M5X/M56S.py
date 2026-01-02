3c601497e979cf6ef572c7d6124a829718c1a105
Phx <apguachingtermort@outlook.com>
2026-01-02 14:23:07 -0600

    update

---
+++ pyasic/miners/whatsminer/btminer/M5X/M56S.py
@@ -0,0 +1,20 @@
+from pyasic.miners.backends import M5X
+from pyasic.miners.device.models import M56SVH30
+
+
+class BTMinerM56SVH30(M5X, M56SVH30):
+    pass
+
+
+from pyasic.miners.device.models import M56SVJ30
+
+
+class BTMinerM56SVJ30(M5X, M56SVJ30):
+    pass
+
+
+from pyasic.miners.device.models import M56SVJ40
+
+
+class BTMinerM56SVJ40(M5X, M56SVJ40):
+    pass
