f1ec23334a066a9b3063cb454c0fd5c230dd4e7c
Phx <apguachingtermort@outlook.com>
2026-01-02 14:22:12 -0600

    update

---
+++ pyasic/miners/whatsminer/btminer/M3X/M31H.py
@@ -0,0 +1,13 @@
+from pyasic.miners.backends import M3X
+from pyasic.miners.device.models import M31HV10
+
+
+class BTMinerM31HV10(M3X, M31HV10):
+    pass
+
+
+from pyasic.miners.device.models import M31HV40
+
+
+class BTMinerM31HV40(M3X, M31HV40):
+    pass
