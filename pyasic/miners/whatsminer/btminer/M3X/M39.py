4ff4bc20ba4e114cb670331a04539236065b38c2
Phx <apguachingtermort@outlook.com>
2026-01-02 14:22:39 -0600

    update

---
+++ pyasic/miners/whatsminer/btminer/M3X/M39.py
@@ -0,0 +1,20 @@
+from pyasic.miners.backends import M3X
+from pyasic.miners.device.models import M39V10
+
+
+class BTMinerM39V10(M3X, M39V10):
+    pass
+
+
+from pyasic.miners.device.models import M39V20
+
+
+class BTMinerM39V20(M3X, M39V20):
+    pass
+
+
+from pyasic.miners.device.models import M39V30
+
+
+class BTMinerM39V30(M3X, M39V30):
+    pass
