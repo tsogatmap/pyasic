538d636213b867581834bd1e013dcd3b7003c303
Phx <apguachingtermort@outlook.com>
2026-01-02 14:23:44 -0600

    update

---
+++ pyasic/miners/whatsminer/btminer/M6X/M66.py
@@ -0,0 +1,27 @@
+from pyasic.miners.backends import M6X
+from pyasic.miners.device.models import M66VK20
+
+
+class BTMinerM66VK20(M6X, M66VK20):
+    pass
+
+
+from pyasic.miners.device.models import M66VK30
+
+
+class BTMinerM66VK30(M6X, M66VK30):
+    pass
+
+
+from pyasic.miners.device.models import M66VL20
+
+
+class BTMinerM66VL20(M6X, M66VL20):
+    pass
+
+
+from pyasic.miners.device.models import M66VL30
+
+
+class BTMinerM66VL30(M6X, M66VL30):
+    pass
