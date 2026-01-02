bf1dc51aab66032d1fee31682875cda1d2c1bee2
Phx <apguachingtermort@outlook.com>
2026-01-02 14:08:26 -0600

    update

---
+++ pyasic/device/algorithm/hashrate/x11.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from typing_extensions import Self
+
+from pyasic.device.algorithm.hashrate.base import AlgoHashRateType
+from pyasic.device.algorithm.hashrate.unit.x11 import X11Unit
+
+from .unit import HashUnit
+
+
+class X11HashRate(AlgoHashRateType[X11Unit]):
+    rate: float
+    unit: X11Unit = HashUnit.X11.default
+
+    def into(self, other: X11Unit) -> Self:
+        return self.__class__(
+            rate=self.rate / (other.value / self.unit.value), unit=other
+        )
