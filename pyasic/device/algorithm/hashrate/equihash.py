61ebae60d1d8270d001f17d263cc29470c915c3e
Phx <apguachingtermort@outlook.com>
2026-01-02 14:08:12 -0600

    update

---
+++ pyasic/device/algorithm/hashrate/equihash.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from typing_extensions import Self
+
+from pyasic.device.algorithm.hashrate.base import AlgoHashRateType
+from pyasic.device.algorithm.hashrate.unit.equihash import EquihashUnit
+
+from .unit import HashUnit
+
+
+class EquihashHashRate(AlgoHashRateType[EquihashUnit]):
+    rate: float
+    unit: EquihashUnit = HashUnit.EQUIHASH.default
+
+    def into(self, other: EquihashUnit) -> Self:
+        return self.__class__(
+            rate=self.rate / (other.value / self.unit.value), unit=other
+        )
