6590421c9708872ae4e1a6633f1edde20c1b165f
Phx <apguachingtermort@outlook.com>
2026-01-02 14:21:45 -0600

    update

---
+++ pyasic/miners/whatsminer/btminer/M2X/M20P.py
@@ -0,0 +1,13 @@
+from pyasic.miners.backends import M2X
+from pyasic.miners.device.models import M20PV10
+
+
+class BTMinerM20PV10(M2X, M20PV10):
+    pass
+
+
+from pyasic.miners.device.models import M20PV30
+
+
+class BTMinerM20PV30(M2X, M20PV30):
+    pass
