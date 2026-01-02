f49fac2a4243549a6e93506493dd577848fd5689
Phx <apguachingtermort@outlook.com>
2026-01-02 14:18:13 -0600

    update

---
+++ pyasic/miners/device/models/whatsminer/M2X/__init__.py
@@ -0,0 +1,8 @@
+from .M20 import M20V10
+from .M20P import M20PV10, M20PV30
+from .M20S import M20SV10, M20SV20, M20SV30
+from .M20S_Plus import M20SPlusV30
+from .M21 import M21V10
+from .M21S import M21SV20, M21SV60, M21SV70
+from .M21S_Plus import M21SPlusV20
+from .M29 import M29V10
